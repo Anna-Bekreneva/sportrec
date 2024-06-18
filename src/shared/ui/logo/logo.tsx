6b136fb76fc159108fb46c0955edf137298a1cda
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-19 00:32:52 +0300

    add logo component

---
+++ src/shared/ui/logo/logo.tsx
@@ -0,0 +1,12 @@
+import { Link as RouterLink } from 'react-router-dom'
+
+import { AppRoutes } from '@/shared'
+import { Link } from '@mui/material'
+
+export const Logo = () => {
+  return (
+    <Link component={RouterLink} to={AppRoutes.MAIN}>
+      <img alt={'logo'} height={32} src={'./logo.svg'} width={130} />
+    </Link>
+  )
+}
