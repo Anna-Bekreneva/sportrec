22dc5d1fbc18669639c736d758df57db686c21fe
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-19 20:20:41 +0300

    add mobileNavItems

--- src/widgets/header/model/index.ts
+++ src/widgets/header/model/index.ts
@@ -1,5 +1,5 @@
 export { getFlag } from './get-flag'
 export type { Language, LanguagesValue } from './language-items'
 export { languageItems, languages } from './language-items'
-export { navItems } from './nav-items'
+export { mobileNavItems, navItems } from './nav-items'
 export { profileNavItems } from './profile-nav-items'
--- src/widgets/header/model/nav-items.ts
+++ src/widgets/header/model/nav-items.ts
@@ -7,3 +7,12 @@ export const navItems: NavLink[] = [
   { href: AppRoutes.COMPETITIONS, name: 'Cоревнования' },
   { href: AppRoutes.ORGANIZATIONS, name: 'Организации' },
 ]
+
+export const mobileNavItems: NavLink[] = [
+  { href: AppRoutes.MAIN, name: 'Лента' },
+  { href: AppRoutes.MARKETPLACE, name: 'Маркетплейс' },
+  { href: AppRoutes.NOTIFICATIONS, name: 'Уведомления' },
+  { href: AppRoutes.RATINGS, name: 'Рейтинги' },
+  { href: AppRoutes.COMPETITIONS, name: 'Cоревнования' },
+  { href: AppRoutes.ORGANIZATIONS, name: 'Организации' },
+]
